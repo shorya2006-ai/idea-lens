e43b2b903786aad526e83a37cb5b920cc136c058
Shorya Jain <jainshorya50@gmail.com>
2026-07-18 21:02:50 +0530

    Add functions to save and add idea status

--- app.py
+++ app.py
@@ -167,3 +167,14 @@ with open(IDEA_STATUS_FILE, "r") as f:
 return json.load(f)
 except:
 return {}
+def save_idea_status(data):
+with open(IDEA_STATUS_FILE, "w") as f:
+json.dump(data, f, indent=4)
+def add_idea_status(file_name, employee):
+data = load_idea_status()
+data[file_name] = {
+"employee": employee,
+"status": "Pending",
+"viewed": False,
+"employee_notified": True
+}
