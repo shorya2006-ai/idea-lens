8d29357c26bb7fc9034a8e1f4d2d2a25444a51c8
Shorya Jain <jainshorya50@gmail.com>
2026-07-24 19:51:10 +0530

    Update history_manager.py

--- history_manager.py
+++ history_manager.py
@@ -1,2 +1,11 @@
 import json
 import os
+#----------Search History--------
+
+SEARCH_HISTORY_FILE = "search_history.json"
+
+
+def load_search_history():
+
+    if not os.path.exists(SEARCH_HISTORY_FILE):
+        return {}
