84e2be30590b462d8de9416348548cb873d99f4b
Shorya Jain <jainshorya50@gmail.com>
2026-07-24 19:53:17 +0530

    Update status_manager.py

--- status_manager.py
+++ status_manager.py
@@ -1,2 +1,10 @@
 import json
 import os
+#----------------IDEA STATUS--------------
+IDEA_STATUS_FILE = "idea_status.json"
+
+
+def load_idea_status():
+
+    if not os.path.exists(IDEA_STATUS_FILE):
+        return {}
